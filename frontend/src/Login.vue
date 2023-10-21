228edc26b2ed1472640d750d68f2c90f283d7a74
sudgr <122168129+sudgr@users.noreply.github.com>
2023-10-21 14:27:59 +0700

    Styles

--- frontend/src/Login.vue
+++ frontend/src/Login.vue
@@ -8,19 +8,21 @@
   </header>
 
   <main>
-    <div>
-      <label for="login">Логин</label>
-      <input id="login" type="text" v-model="login">
+    <div class="auth">
+      <div class="input-auth">
+        <label for="login">Логин</label>
+        <input id="login" type="text" v-model="login">
+      </div>
+      <div class="input-auth">
+        <label for="login">Пароль</label>
+        <input id="password" type="password" v-model="password">
+      </div>
+      <div class="input-auth">
+        <input id="create-new-user" type="checkbox" v-model="checkbox">
+        <label for="create-new-user">Создать нового пользователя</label>
+      </div>
+      <button @click="onClick">Войти</button>
     </div>
-    <div>
-      <label for="login">Пароль</label>
-      <input id="password" type="password" v-model="password">
-    </div>
-    <div>
-      <input id="create-new-user" type="checkbox" v-model="checkbox">
-      <label for="create-new-user">Создать нового пользователя</label>
-    </div>
-    <button @click="onClick">Войти</button>
   </main>
 </template>
 
@@ -43,6 +45,29 @@ function onClick() {
 <style scoped>
 header {
   line-height: 1.5;
+}
+main {
+    display: flex;
+    width: 100%;
+    justify-content: space-around;
+}
+.auth {
+  height: 266px;
+  display: flex;
+  width: 300px;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+  justify-content: center;
+  border: 1px solid gray;
+  border-radius: 20px;
+}
+
+.input-auth {
+  width: 100%;
+  gap: 20px;
+  display: flex;
+  justify-content: center;
 }
 
 .logo {
