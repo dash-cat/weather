fc88a275854c1f1ad0dd4088f285f916431cb174
greggy <sudakovgrigory@gmail.com>
2023-12-05 19:10:34 +0700

    init commit

--- frontend/dist/assets/main-bc5a4f45.js
+++
@@ -1,2 +0,0 @@
-import{f as a,W as p}from"./Weather-2c9f8449.js";a(p).mount("#app");
-//# sourceMappingURL=main-bc5a4f45.js.map
--- frontend/src/main.js
+++
@@ -1,6 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import Login from './Login.vue'
-
-createApp(Login).mount('#app')
